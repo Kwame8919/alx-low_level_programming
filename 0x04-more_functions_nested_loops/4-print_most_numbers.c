2b3edc2977822ccc11a2bdc399850da34c03abcf
Kwame8919 <101393080+Kwame8919@users.noreply.github.com>
2022-04-14 12:25:27 +0000

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,13 +4,17 @@
  * print_numbers - prints  the numbers, from 0 to 9 except 2,4
  * followed by a new line
  */
-void print_most_numbers(void){
-    for(int i=48;i<=57;i++)
-    {
-        if(i!=50 && i!=52)
-        {
-            _putchar(i);
-        }
-    }
-    _putchar('\n')
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+		}
+	}
+
+	_putchar('\n');
 }
