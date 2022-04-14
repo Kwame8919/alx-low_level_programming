88227d89b12ccf9cb25ab13076467faa94265442
Kwame8919 <101393080+Kwame8919@users.noreply.github.com>
2022-04-14 14:04:27 +0000

    Create 10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - prints a triangle, followed by a new line
+ * @size: size of the triangle
+ */
+void print_triangle(int size)
+{
+    if(size>=0)
+    {
+        _putchar('\n');
+    }
+    for(int i=0; i<size; i++)
+    {
+        for(int j=0; j<=i; j++)
+        {
+            _putchar('#');
+        }
+        _putchar('\n');
+    }
+}
